87e3353d83ec082fb68cc4a4e6efc54b554cfcf0
aryelzx <arielcearamor102@gmail.com>
2024-08-18 18:17:24 -0300

    feat(notifications): create a useHook to configure Expo Notifications

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,41 @@
-import { Text, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  Text,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+} from "react-native";
+import {
+  PushNotificationState,
+  usePushNotifications,
+} from "../../shared/hooks/usePushNotifications";
 
 export const HomeScreen = () => {
+  const { expoPushToken, notification, sendPushNotification } =
+    usePushNotifications();
+  const data = JSON.stringify(notification, undefined, 2);
+
   return (
     <>
-      <View>
-        <Text>HomeScreen!!!!</Text>
+      <View style={styles.container}>
+        <Text>Token: {expoPushToken}</Text>
+        <Text style={{ color: "#000" }}>{data}</Text>
+        <Button
+          title="Send notification"
+          onPress={() => sendPushNotification}
+        />
       </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    maxHeight: 900,
+    overflow: "scroll"
+  },
+});
